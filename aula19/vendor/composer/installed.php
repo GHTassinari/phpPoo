03cc491e5580b191aafce37809c8209d3496cc05
Guilherme Tassinari <guilhermehen.tassinari@gmail.com>
2025-01-08 01:52:02 -0300

    Testando o Slugify com o Composer

--- aula19/vendor/composer/installed.php
+++ aula19/vendor/composer/installed.php
@@ -1,6 +1,6 @@
 <?php return array(
     'root' => array(
-        'name' => 'nodestudio/composer',
+        'name' => 'usuario/aula19',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
         'reference' => 'f048784637f8f7907a3c80ce857061525fe08dd2',
@@ -10,21 +10,21 @@
         'dev' => true,
     ),
     'versions' => array(
-        'nodestudio/composer' => array(
+        'cocur/slugify' => array(
+            'pretty_version' => 'v3.2',
+            'version' => '3.2.0.0',
+            'reference' => 'd41701efe58ba2df9cae029c3d21e1518cc6780e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../cocur/slugify',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'usuario/aula19' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
             'reference' => 'f048784637f8f7907a3c80ce857061525fe08dd2',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
-        'pagseguro/pagseguro-php-sdk' => array(
-            'pretty_version' => '4.2.1',
-            'version' => '4.2.1.0',
-            'reference' => '3e164aff66fa089eb79a80ce6b7413fe36aa9b95',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../pagseguro/pagseguro-php-sdk',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
