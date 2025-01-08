f048784637f8f7907a3c80ce857061525fe08dd2
Guilherme Tassinari <guilhermehen.tassinari@gmail.com>
2025-01-08 01:39:55 -0300

    Utilizando o composer através do Terminal

---
+++ aula18/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nodestudio/composer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '68e537929654d467b047ab6bb9ff2640895541be',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nodestudio/composer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '68e537929654d467b047ab6bb9ff2640895541be',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pagseguro/pagseguro-php-sdk' => array(
+            'pretty_version' => '4.2.1',
+            'version' => '4.2.1.0',
+            'reference' => '3e164aff66fa089eb79a80ce6b7413fe36aa9b95',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../pagseguro/pagseguro-php-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
